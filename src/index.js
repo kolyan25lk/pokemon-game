e28f645029595ca32b2bcd32afd3446902ba4333
Николай Довгий <you@example.com>
2021-03-10 23:37:45 +0200

    edit link home page2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  //  const el = React.createElement(
+ //   'h1',
+ //   null,
+ // 'Hello world, React.js!!!!'
+// );
+const el = <h1>Hello World!</h1>
+console.log(el);
+ReactDOM.render(el, document.getElementById('root'));
