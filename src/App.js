22b6d97f1ab26c1b11b01a7cd1f3cabeb96ac763
kolyan25lk <kolyan25lk@gmail.com>
2021-03-12 22:47:51 +0200

    home2_po_video

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderBlock from "./components/HeaderBlock"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-        
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <>
+    <HeaderBlock title= "This is new Title"
+    descr="This is new description!"
+    />
+    <HeaderBlock title= "This is new Title"
+    hideBackground
+    />
+    <Layout title= "This is new Layout"
+    
+    />
+    
+
+</>
+  )
 }
 
 export default App;
